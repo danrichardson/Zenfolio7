6091fa3670f2372a91ded01e3944e1970991edfe
George P. Plympton <plymptonia@yahoo.com>
2019-04-03 16:05:26 -0700

    Getting upload tasks started.  Will create a new Control that shows the upload progress and have a Parallel.ForEach or something similar with a max number of upload threads.

--- Zenfolio7/Zensync/ViewModel/ZenSyncViewModel.cs
+++ Zenfolio7/Zensync/ViewModel/ZenSyncViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using MVVMLight.Messaging;
 using Zenfolio7.Messages;
 using Zenfolio7.View.ViewModel;
@@ -14,21 +17,131 @@ namespace Zenfolio7.ZenSync
 
         public GroupElement SelectedGroupElement { get; set; }
         public string SelectedDirectory { get; set; }
+        public string SyncProcessUpdate { get; set; }
         public RelayCommand SyncCommand { get; set; }
+        public string SyncButtonText { get; set; }
+        public bool IsSyncing { get; set; }
+        public RelayCommand CancelCommand { get; set; }
+        public string CancelButtonText { get; set; }
+        private CancellationTokenSource _cts;
+        private static Thread _uploadThread;
         public ZenSyncViewModel() 
         {
             Messenger.Default.Register<DataUpdateMessage>(this, UpdateData);
             SyncCommand = new RelayCommand(Sync, CanSync);
+            CancelCommand = new RelayCommand(Cancel, CanCancel);
+
+            SyncButtonText = "Sync!";
+            CancelButtonText = "Cancel";
         }
 
         private bool CanSync(object obj)
         {
-            return Directory.Exists(SelectedDirectory) && SelectedGroupElement != null;
+            return Directory.Exists(SelectedDirectory) && SelectedGroupElement != null && !IsSyncing;
         }
 
-        private void Sync(object obj)
+        private async void Sync(object obj)
+        {
+            IsSyncing = true;
+            SyncProcessUpdate = string.Empty;
+            TaskCompletionSource<int> tcs = new TaskCompletionSource<int>();
+            _cts = new CancellationTokenSource();
+            //_cts.Token.Register(() =>
+            //{
+            //    tcs.TrySetCanceled();    
+            //});
+
+            //using (var _cts = new CancellationTokenSource())
+            {
+                await DirectoryIteratoryAsync(_cts);
+            }
+            
+            //var completedTask = await Task.WhenAny(task, tcs.Task);
+
+            //try
+            //{
+            //    await DirectoryIteratoryAsync();
+            //}
+            //catch (TaskCanceledException)
+            //{
+
+            //}
+
+        }
+
+        private bool CanCancel(object obj)
+        {
+            return IsSyncing;
+        }
+
+        private void Cancel(object obj)
+        {
+            // Cancel the task
+            if (_cts != null)
+            {
+                _cts.Cancel();
+            }
+            if (_uploadThread != null)
+            {
+                _uploadThread.Abort();
+            }
+        }
+        private Task DirectoryIteratoryAsync(CancellationTokenSource cts)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    //using (cts.Token.Register(Thread.CurrentThread.Abort))
+                    {
+                        var dirContents = Directory.GetDirectories(SelectedDirectory, "*.*", SearchOption.AllDirectories).ToList();
+                        dirContents.AddRange(Directory.GetFiles(SelectedDirectory, "*.*", SearchOption.AllDirectories).ToList());
+                        foreach (var dir in dirContents)
+                        {
+                            if (cts.IsCancellationRequested)
+                            {
+                                throw new TaskCanceledException($"Task Cancelled on dir {dir}");
+                            }
+                            _uploadThread = SyncFileThread(dir, 300);
+                            _uploadThread.Start();
+                            SyncProcessUpdate += dir + "\n";
+                        }
+                    }
+                }
+                catch (TaskCanceledException)
+                {
+                    //do something?
+                }
+                catch (ThreadAbortException)
+                {
+                    //do something else
+                }
+                catch (Exception ex)
+                {
+                    //do something else
+                }
+                finally
+                {
+                    IsSyncing = false;
+                }
+            });
+        }
+
+        private Thread SyncFileThread(string dir, int time)
         {
             
+            return new Thread(() =>
+            {
+                try
+                {
+                    Task.Delay(time).Wait();
+                }
+                catch (Exception ex)
+                {
+                    //do something
+                    throw ex;
+                }
+            });
         }
 
         private void UpdateData(DataUpdateMessage obj)
